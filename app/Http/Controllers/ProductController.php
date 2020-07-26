c545e35bb35f20ac8aaa8d47f2a087a2489f19b2
MohammedEl-Saeed <mohammedelsaeed4444@gmail.com>
2020-07-26 19:52:16 +0200

    add comment

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,36 +21,36 @@ class ProductController extends Controller
     }
 
 
-    public function addToCart($id)
-    {
-        $product = Product::find($id);
-        if(!$product) {
-            abort(404);
-        }
-        $cartItem = [
-            "name" => $product->name,
-            "quantity" => 1,
-            "price" => $product->price,
-            "photo" => $product->photo
-        ];
-        $cart = session()->get('cart');
-        // if cart is empty then this the first product
-        if(!$cart) {
-            $cart = [
-                $id => $cartItem
-            ];
-            session()->put('cart', $cart);
-        }
-        // if cart not empty then check if this product exist then increment quantity
-        if(isset($cart[$id])) {
-            $cart[$id]['quantity']++;
-            session()->put('cart', $cart);
-        } else {
-            // if item not exist in cart then add to cart with quantity = 1
-            $cart[$id] = $cartItem;
-
-            session()->put('cart', $cart);
-        }
-        return redirect()->back()->with('success', 'Product added to cart successfully!');
-    }
+//    public function addToCart($id)
+//    {
+//        $product = Product::find($id);
+//        if(!$product) {
+//            abort(404);
+//        }
+//        $cartItem = [
+//            "name" => $product->name,
+//            "quantity" => 1,
+//            "price" => $product->price,
+//            "photo" => $product->photo
+//        ];
+//        $cart = session()->get('cart');
+//        // if cart is empty then this the first product
+//        if(!$cart) {
+//            $cart = [
+//                $id => $cartItem
+//            ];
+//            session()->put('cart', $cart);
+//        }
+//        // if cart not empty then check if this product exist then increment quantity
+//        if(isset($cart[$id])) {
+//            $cart[$id]['quantity']++;
+//            session()->put('cart', $cart);
+//        } else {
+//            // if item not exist in cart then add to cart with quantity = 1
+//            $cart[$id] = $cartItem;
+//
+//            session()->put('cart', $cart);
+//        }
+//        return redirect()->back()->with('success', 'Product added to cart successfully!');
+//    }
 }
